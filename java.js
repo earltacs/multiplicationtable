1643c3e65d8535c5c44f2f17c963f4849572b38c
earltacs <34850102+earltacs@users.noreply.github.com>
2018-01-18 15:03:42 +0800

    Update

--- java.js
+++ java.js
@@ -1,34 +1,31 @@
-var result = '';
 
-function buff(val){
-  var buff = '';
-  var pad = 4 - val;
-    while( pad-- > 0 )
-            buff += ' ';            
-  return buff;
-}
-
-function generate_table() {
-  	var color_td,
-  	rows = document.getElementById('rows').value,
-  	columns = document.getElementById('columns').value;
-	document.write("<table border='1px'>");
-	columns++, rows++;
-	for(var i = 0; i < columns; i++) {
-
-		document.write("<tr style='height:30px;'>");
-
-		for(var j = 0; j < rows; j++) {
-
-			if(j>0&&i>0)
-			{
-				color_td = "#FFA500";
-				document.write("<td style='width:30px;background-color:" + color_td + "'>" + i*j + "</td>");
-			}
+function generateTable(){
+	var rows = parseInt(document.getElementById('rows').value);
+	var columns = parseInt(document.getElementById('columns').value);
+	rows++; columns++;
+	
+	var htmlContainer = "<table border='1' id='tab' style='border-color: #848484; border-style: none; width: 100px; height: 100px; margin'>";
+	
+	for (var i = 1; i < columns; i++) {
+		htmlContainer += "<tr>"
+		for (var j = 1; j < rows; j++) {
+			htmlContainer += "<td id ='tab1'>" + (i*j) + "</td>"
 		}
-		document.write("</tr>");
+		htmlContainer += "</tr>";
 	}
 
-	document.write("</table>");
+	document.getElementById('table').innerHTML = htmlContainer;	
 }
 
+function bgColorChange(){
+	var color = document.getElementById('bgcolor').value;
+	
+	document.getElementById('tab').style.backgroundColor = color;
+	document.getElementById('tab').style.borderColor = color;
+}
+
+function txtColorChange(){
+	var color = document.getElementById('txtcolor').value;
+	
+	document.getElementById('tab').style.color = color;
+}
